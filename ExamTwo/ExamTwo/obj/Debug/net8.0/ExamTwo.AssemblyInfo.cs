eeb3da6feb684a78193aa2fc82d8b5a339501839
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-12-05 16:40:37 -0600

    Cambios(api): Se agrega el controlador de máquina de café - GET /api/coffees: retorna cafés disponibles - GET /api/coins: retorna monedas disponibles - POST /api/calculate-total: calcula el costo total de la orden - POST /api/buy: procesa la compra, valida pago y retorna cambio

--- ExamTwo/ExamTwo/obj/Debug/net8.0/ExamTwo.AssemblyInfo.cs
+++ ExamTwo/ExamTwo/obj/Debug/net8.0/ExamTwo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59e987c61da2065310da2c01f9cf4e3082bc82c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a0dd1b97a26c10ecd78a47d09139727cb0bb96")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
